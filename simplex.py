94083288db3da1c2be2455853aa3906bf054f792
vlad <vladk6813@gmail.com>
2024-10-11 17:48:02 +0300

    .

--- simplex.py
+++ simplex.py
@@ -1,43 +1,32 @@
 def input_lpp():
-    # Input number of variables and constraints
-    n = int(input("Enter the number of variables: "))
     m = int(input("Enter the number of constraints: "))
 
-    # Input coefficients of the objective function
     print("Enter the coefficients of the objective function (space-separated):")
     c = list(map(float, input().split()))
 
-    # Input coefficients of the constraints row-wise
     print("Enter the coefficients of the constraints row-wise (Ax ≤ b):")
-    A = []
+    a = []
     for i in range(m):
         row = list(map(float, input().split()))
-        A.append(row)
+        a.append(row)
 
-    # Input the right-hand side values
     print("Enter the right-hand side values (b):")
     b = list(map(float, input().split()))
 
-    # Input the approximation accuracy (epsilon)
     epsilon = float(input("Enter the approximation accuracy (epsilon): "))
 
-    return A, b, c, epsilon
+    return a, b, c, epsilon
 
 
-def construct_tableau(A, b, c):
-    # Initializing the tableau
-    num_constraints = len(A)
-    num_variables = len(c)
+def construct_tableau(a, b, c):
+    num_constraints = len(a)
 
-    # Create the tableau with slack variables
     tableau = []
 
-    # Add each constraint row (A_i | slack variables | b_i)
     for i in range(num_constraints):
-        row = A[i] + [1 if j == i else 0 for j in range(num_constraints)] + [b[i]]
+        row = a[i] + [1 if j == i else 0 for j in range(num_constraints)] + [b[i]]
         tableau.append(row)
 
-    # Add the objective function row (c | slack variables | 0)
     objective_row = [-x for x in c] + [0] * (num_constraints + 1)
     tableau.append(objective_row)
 
@@ -51,11 +40,10 @@ def print_tableau(tableau):
     print()
 
 def find_pivot_column(tableau, epsilon):
-    # Find the most negative value in the objective row (excluding the last column)
     objective_row = tableau[-1]
     min_value = min(objective_row[:-1])
     if min_value >= -epsilon:
-        return -1  # If all values are non-negative or close to zero, the solution is optimal
+        return -1
     return objective_row.index(min_value)
 
 def find_pivot_row(tableau, pivot_col, epsilon):
@@ -63,7 +51,7 @@ def find_pivot_row(tableau, pivot_col, epsilon):
     min_ratio = float('inf')
     pivot_row = -1
     for i in range(num_rows):
-        if tableau[i][pivot_col] > epsilon:  # Only consider positive entries in the pivot column
+        if tableau[i][pivot_col] > epsilon:
             ratio = tableau[i][-1] / tableau[i][pivot_col]
             if ratio < min_ratio:
                 min_ratio = ratio
@@ -81,3 +69,50 @@ def pivot_operation(tableau, pivot_row, pivot_col):
         if i != pivot_row:
             row_factor = tableau[i][pivot_col]
             tableau[i] = [tableau[i][j] - row_factor * tableau[pivot_row][j] for j in range(num_cols)]
+
+
+def simplex_method(tableau, epsilon):
+    while True:
+        pivot_col = find_pivot_column(tableau, epsilon)
+        if pivot_col == -1:
+            break
+
+        pivot_row = find_pivot_row(tableau, pivot_col, epsilon)
+        if pivot_row == -1:
+            return "The method is not applicable!"
+
+        pivot_operation(tableau, pivot_row, pivot_col)
+
+    return tableau
+
+
+def extract_solution(tableau, num_variables):
+    num_constraints = len(tableau) - 1
+    solution = [0] * num_variables
+    for i in range(num_constraints):
+        non_zero_col = [j for j in range(num_variables) if tableau[i][j] == 1]
+        if len(non_zero_col) == 1 and tableau[i][non_zero_col[0]] == 1:
+            solution[non_zero_col[0]] = tableau[i][-1]
+    return solution
+
+
+def solve_lpp():
+    a, b, c, epsilon = input_lpp()
+
+    tableau = construct_tableau(a, b, c)
+    print_tableau(tableau)
+
+    result = simplex_method(tableau, epsilon)
+
+    if isinstance(result, str):
+        print(result)
+    else:
+        optimal_value = result[-1][-1]
+        num_variables = len(c)
+        solution = extract_solution(result, num_variables)
+        print("Optimal solution (X*):", solution)
+        print("Optimal value:", optimal_value)
+
+
+if __name__ == "__main__":
+    solve_lpp()
